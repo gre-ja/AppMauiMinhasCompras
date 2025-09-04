1a588230bfb8ea02a8b0f636fca949078bc9861c
Grécia <Grcia@DESKTOP-5KO6I9M>
2025-09-04 20:35:34 -0300

    atual

--- AppMauiMinhasCompras/Views/ListaProduto.xaml.cs
+++ AppMauiMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using AppMauiMinhasCompras.Models;
 
 namespace AppMauiMinhasCompras.Views;
@@ -14,15 +15,22 @@ public partial class ListaProduto : ContentPage
 		lst_produtos.ItemsSource = lista;
 	}
 
-    protected async override void OnAppearing()
+	protected async override void OnAppearing()
 	//onappearing sempre chamado quando uma tela aparece, toda vez que a tela aparecer,
 	//vai no sqlite buscar na lista o produto e abastecer na observablecollection
-    {
-		List<Produto> tmp = await App.Db.GetAll();
+	{
+		try
+		{
+			List<Produto> tmp = await App.Db.GetAll();
 
-		tmp.ForEach(i => lista.Add(i));
-    }
+			tmp.ForEach(i => lista.Add(i));
 
+		}catch (Exception ex)
+        {
+			await DisplayAlert("Ops", ex.Message, "OK");
+
+		}
+	}
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
 		try
@@ -36,13 +44,20 @@ public partial class ListaProduto : ContentPage
     }
 	private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
 	{
-		string q = e.NewTextValue;
+		try
+		{
+			string q = e.NewTextValue;
 
-		lista.Clear();
+			lista.Clear();
 
-        List<Produto> tmp = await App.Db.Search(q);
+			List<Produto> tmp = await App.Db.Search(q);
 
-        tmp.ForEach(i => lista.Add(i));
+			tmp.ForEach(i => lista.Add(i));
+
+		}catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
 
     }
 
@@ -55,8 +70,40 @@ public partial class ListaProduto : ContentPage
 		DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
-    private void MenuItem_Clicked(object sender, EventArgs e)
+    private async void MenuItem_Clicked(object sender, EventArgs e)
     {
-        lista.Clear();
+		try
+		{
+			MenuItem selecionado = sender as MenuItem;
+
+			Produto p = selecionado.BindingContext as Produto;
+			bool confirm = await DisplayAlert("Tem certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+			if (confirm)
+			{
+				await App.Db.Delete(p.Id);
+				lista.Remove(p);
+			}
+
+		}catch (Exception ex)
+		{
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		try 
+		{
+			Produto p = e.SelectedItem as Produto;
+			Navigation.PushAsync(new Views.EditarProduto
+			{
+				BindingContext = p,
+			});
+
+        }catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
--- AppMauiMinhasCompras/Views/NovoProduto.xaml.cs
+++ AppMauiMinhasCompras/Views/NovoProduto.xaml.cs
@@ -30,4 +30,5 @@ public partial class NovoProduto : ContentPage
 		}
 
     }
+    
 }
