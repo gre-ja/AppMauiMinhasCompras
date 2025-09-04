8af4d2634bc6038ef52c057a34cfdbfef5b134f2
Grécia <Grcia@DESKTOP-5KO6I9M>
2025-09-04 20:55:23 -0300

    produto.cs e listaProduto.cs

--- AppMauiMinhasCompras/Views/ListaProduto.xaml.cs
+++ AppMauiMinhasCompras/Views/ListaProduto.xaml.cs
@@ -21,6 +21,8 @@ public partial class ListaProduto : ContentPage
 	{
 		try
 		{
+			lista.Clear();//correção da duplicação de dados ao inserir
+
 			List<Produto> tmp = await App.Db.GetAll();
 
 			tmp.ForEach(i => lista.Add(i));
